713e10e2d28d294e3c81c8e231112458cf8603a5
root <root@vm4336611220.bitcommand.com>
2021-10-20 15:21:03 +0330

    first commit

---
+++ deployment/app.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+for dir in "storage" "storage/framework" \
+    "storage/framework/cache" "storage/framework/cache/data" \
+    "storage/framework/sessions" \
+    "storage/framework/testing" \
+    "storage/framework/views" \
+    "storage/app" "storage/app/public" \
+    "storage/logs"
+do
+    if [[ ! -d "$dir" ]]; then
+        mkdir "$dir"
+    fi
+done
+
+if [[ ! -f .env ]]; then
+    cp .env.example .env
+fi
+
+if [[ ! -d vendor ]]; then
+    composer install --no-progress
+fi
+
+chown -R www-data:www-data storage bootstrap
+
+chmod a+rx /var/www/deployment/wait-for-it.sh
+bash /var/www/deployment/wait-for-it.sh -t 30 -s web-koodak_wp-db_1:3306 -- echo "Database available, continuing."
+
+php artisan migrate --force
+php artisan key:generate
+
+if [[ ! -f storage/oauth-private.key ]]; then
+    php artisan passport:install
+fi
+
+php artisan config:cache
+php artisan route:cache
+
+exec php-fpm
+
